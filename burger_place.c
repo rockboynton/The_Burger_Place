dc7f297b45ec75dde5b704e2a58763e79ddd67cb
Rock Boynton <boyntonrl@msoe.edu>
2019-10-17 11:38:48 -0500

    parse_input WIP

--- burger_place.c
+++ burger_place.c
@@ -5,9 +5,24 @@
 static void parse_input(FILE* input);
 
 void simulate(FILE* input) {
+    burger_place_init();
     parse_input(input);
 }
 
+void burger_place_init() {
+
+}
+
 static void parse_input(FILE* input) {
+    fscanf(input, "%d", &(burgers->burger_cooks));
+    fscanf(input, "%d", &(burgers->burger_cook_time));
+    fscanf(input, "%d", &(burgers->burgers_per_cook));
+
+    fscanf(input, "%d", &(fries->fryers));
+    fscanf(input, "%d", &(fries->fry_serving_cook_time));
+    fscanf(input, "%d", &(fries->fry_servings_per_cook));
+
+    fscanf(input, "%d", &(customers->num_customers));
     
+    // TODO for loop here
 }
--- burger_place.h
+++ burger_place.h
@@ -7,36 +7,45 @@
 #include <stdio.h>
 #include <semaphore.h>
 
-ll_t burger_tray;
-ll_t fry_tray;
+ll_t* burger_tray;
+ll_t* fry_tray;
 
 sem_t burgers_ready;
 sem_t fries_ready;
+
+typedef struct {
+    int burger_cooks;
+    int burger_cook_time; // microseconds
+    int burgers_per_cook;
+} Burgers;
+
+typedef struct {
+    int fryers;
+    int fry_serving_cook_time; // microseconds
+    int fry_servings_per_cook;
+} Fries;
+
+typedef struct {
+    int num_customers;
+    int orders_filled;
+} Customers;
+
+typedef struct {
+    int burgers;
+    int orders_filled;
+} Customer;
 
 Burgers* burgers;
 Fries* fries;
 Customers* customers;
 
-
-typedef struct {
-    int burger_cooks;
-    int burger_cook_time; // microseconds
-    int bugers_per_cook;
-} Burgers;
-
-typedef struct {
-    int fryers;
-    int fry_cook_time; // microseconds
-    int fry_servings_per_cook;
-} Fries;
-
-typedef struct {
-    int customers;
-    int orders_filled;
-} Customers;
-
-
 void simulate(FILE* input);
+
+/**
+ * @brief Initialize warming tray linked lists, semaphores, and all structs
+ * 
+ */
+void burger_place_init();
 
 // Customers:
 
